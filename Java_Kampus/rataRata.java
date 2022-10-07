842534a76cbd0bf7dbe623f8a21d1121625f5ba7
AgungBahtiarr <agungbahtiar050@gmail.com>
2022-10-07 23:14:13 +0700

    hapus scanner

--- Java_Kampus/rataRata.java
+++ Java_Kampus/rataRata.java
@@ -7,15 +7,14 @@ import java.util.Scanner;
  */
 public class rataRata {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         Integer angka1,angka2,angka3;
         angka1=5;
         angka2=2;
         angka3=2;
         
+        System.out.println("Rata rata dari " + angka1 + " + " + angka2 + " + " + angka3 + " adalah...");
         Integer rataRata=(angka1+angka2+angka3)/3;
         System.out.println(rataRata);
-        input.close();
     }
    
 }
